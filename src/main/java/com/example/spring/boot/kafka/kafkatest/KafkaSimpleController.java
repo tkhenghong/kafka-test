c7c5e63bef6ecd96a55d52f35e2204f2fdc44cac
Teoh Kheng Hong <tkhenghong@gmail.com>
2020-03-07 18:42:40 +0800

    First commit

---
+++ src/main/java/com/example/spring/boot/kafka/kafkatest/KafkaSimpleController.java
@@ -0,0 +1,10 @@
+package com.example.spring.boot.kafka.kafkatest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/kafka")
+public class KafkaSimpleController {
+
+}
